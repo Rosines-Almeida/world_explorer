b4621685204c121ee1bf7494986d30bb8c50e13d
Rosinês da Silva Almeida <r.da.silva.almeida@avanade.com>
2025-11-08 01:12:42 -0300

    config build

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,7 +2,15 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
-    path: '**',
+    path: '',
     renderMode: RenderMode.Prerender
+  },
+  {
+    path: 'not-found',
+    renderMode: RenderMode.Prerender
+  },
+  {
+    path: ':cca3',
+    renderMode: RenderMode.Server
   }
 ];
